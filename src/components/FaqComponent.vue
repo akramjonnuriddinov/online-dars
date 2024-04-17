fa49514c885080a556b71978b13844da58581710
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-04-17 21:45:56 +0500

    Add AppAccordion to FaqComponent

--- src/components/FaqComponent.vue
+++ src/components/FaqComponent.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppAccordion from './AppAccordion.vue'
+</script>
 
 <template>
   <section class="faq">
     <div class="container faq__container">
       <h2 class="base-title">ENG KO’P BERILADIGAN <span class="base-title-red">SAVOLLAR</span></h2>
+      <app-accordion />
     </div>
   </section>
 </template>
